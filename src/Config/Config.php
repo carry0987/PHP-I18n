57b82dd2d6967c95af378d6e7da9c3655befe4f8
carry0987 <carry0987@users.noreply.github.com>
2023-12-05 17:51:26 +0800

    Update Config.php

--- src/Config/Config.php
+++ src/Config/Config.php
@@ -47,7 +47,7 @@ class Config
         }
     }
 
-    public static function getOptions(string $key = null, mixed $default = null)
+    public static function getOptions(string $key = null, $default = null)
     {
         if ($key) {
             return self::$config[$key] ?? $default;
